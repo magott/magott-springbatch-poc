bf2eb57eb435885bcdf168f144a191972674fe77
Morten Andersen-Gott <morten.andersengott@gmail.com>
2010-07-28 23:52:04 +0200

    Added actual timeout property to CompletionPolicy

--- src/main/java/no/magott/spring/batch/window/BatchWindowCompletionPolicy.java
+++ src/main/java/no/magott/spring/batch/window/BatchWindowCompletionPolicy.java
@@ -1,5 +1,6 @@
 package no.magott.spring.batch.window;
 
+import java.util.Date;
 import java.util.Random;
 
 import org.apache.commons.logging.Log;
@@ -7,10 +8,14 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.repeat.RepeatContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.util.Assert;
 
-public class BatchWindowCompletionPolicy extends SimpleCompletionPolicy {
+public class BatchWindowCompletionPolicy extends SimpleCompletionPolicy{
 
-    private static final Log log = LogFactory.getLog(BatchWindowCompletionPolicy.class);    
+    private static final Log log = LogFactory.getLog(BatchWindowCompletionPolicy.class);   
+    
+    //Should probably be changed to take a cron expression
+    private Date closingTime;
     
     public boolean isComplete(RepeatContext context) {
         return super.isComplete(context) || isBatchWindowClosed(context);
@@ -21,14 +26,20 @@ public class BatchWindowCompletionPolicy extends SimpleCompletionPolicy {
     }
 
     private boolean isBatchWindowClosed(RepeatContext context) {
-        if(new Random().nextBoolean()){
-            log.debug("Complete");
-            //Terminate only is no different from complete only in the way the step exits.
+        Assert.notNull(closingTime, "ClosingTime must be set");
+    	if(new Date().after(closingTime)){
+            log.debug("Batch window has passed, batch will exit with data left to process");
+            //Seems to be no difference between terminateOnly and completeOnly?
             context.getParent().setTerminateOnly();
             return true;
         }
-        log.debug("Not completed");
+        log.trace("Timeout not reached");
         return false;
     }
+
+	public void setClosingTime(long closingTimeInMillis) {
+		this.closingTime = new Date(closingTimeInMillis);
+	}
+
 
 }
--- src/test/java/no/magott/spring/batch/window/InfiniteJobBatchWindowTests.java
+++ src/test/java/no/magott/spring/batch/window/InfiniteJobBatchWindowTests.java
@@ -2,10 +2,14 @@ package no.magott.spring.batch.window;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,6 +19,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class InfiniteJobBatchWindowTests {
 
+	private static final int DELAY_FOR_BATCHWINDOW_IN_MILLIS = 5000;
+	private static final int TIMEOUT_LENIENCE_IN_MILLIS = 2000;
+	
     @Autowired
     private JobLauncher jobLauncher;
 
@@ -26,9 +33,16 @@ public class InfiniteJobBatchWindowTests {
         assertNotNull(jobLauncher);
     }
     
-    @Test
+    @Test(timeout=TIMEOUT_LENIENCE_IN_MILLIS+DELAY_FOR_BATCHWINDOW_IN_MILLIS)
     public void testLaunchJob() throws Exception {
-        jobLauncher.run(job, new JobParameters());
+        JobParameters params = new JobParametersBuilder().addLong("batchwindow.closingTime", getClosingTime()).toJobParameters();
+    	jobLauncher.run(job, params);
+    }
+    
+    private Long getClosingTime(){
+    	Calendar calendar = Calendar.getInstance();
+    	calendar.add(Calendar.MILLISECOND, DELAY_FOR_BATCHWINDOW_IN_MILLIS);
+    	return (Long)calendar.getTime().getTime();
     }
     
 }
