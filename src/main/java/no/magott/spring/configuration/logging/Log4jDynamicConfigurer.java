e85dd540e9159e313586cd16b0efbdaa8f9bc85a
Morten Andersen-Gott <morten.andersengott@gmail.com>
2010-09-20 21:00:22 +0200

    committing with tests failing, will fix later

--- src/main/java/no/magott/spring/configuration/logging/Log4jDynamicConfigurer.java
+++ src/main/java/no/magott/spring/configuration/logging/Log4jDynamicConfigurer.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Log4jConfigurer;
 import static org.springframework.util.Log4jConfigurer.CLASSPATH_URL_PREFIX;;
 
+/**
+ * Enables automatic reload of log4j configuration similar to org.springframework.web.util.Log4jConfigListener
+ * but can be used outside a web container, such as stand alone JRE applications.
+ * @author morten andersen-gott
+ *
+ */
 public class Log4jDynamicConfigurer implements InitializingBean, DisposableBean{
 
     public static final String DEFAULT_LOG4J_PROPERTIES = CLASSPATH_URL_PREFIX+"log4j.properties";
