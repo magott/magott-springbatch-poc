0cc19c6f4c2904a30ac0f7fd73f35b78d3c6a66f
Morten Andersen-Gott <morten.andersengott@gmail.com>
2010-09-22 16:25:05 +0200

    tidied up

--- src/main/java/no/magott/spring/batch/ExceptionThrowingProcessor.java
+++ src/main/java/no/magott/spring/batch/ExceptionThrowingProcessor.java
@@ -1,14 +1,19 @@
 package no.magott.spring.batch;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 public class ExceptionThrowingProcessor implements ItemProcessor<String, String> {
 
+    private static final Log log = LogFactory.getLog(ExceptionThrowingProcessor.class);
+    
     int count = 0;
     public String process(String arg0) throws Exception {
         if(++count > 12){
             throw new IllegalArgumentException("foo");
         }
+        log.debug("Process count "+count);
         return arg0;
     }
 
