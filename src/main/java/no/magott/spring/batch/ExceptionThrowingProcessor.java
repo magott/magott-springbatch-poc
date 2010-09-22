463b6c15f062232545dacd1c06acfb8c1cf1597d
Morten Andersen-Gott <morten.andersengott@gmail.com>
2010-09-22 16:06:13 +0200

    slightly more sophisticated processor

--- src/main/java/no/magott/spring/batch/ExceptionThrowingProcessor.java
+++ src/main/java/no/magott/spring/batch/ExceptionThrowingProcessor.java
@@ -4,8 +4,12 @@ import org.springframework.batch.item.ItemProcessor;
 
 public class ExceptionThrowingProcessor implements ItemProcessor<String, String> {
 
+    int count = 0;
     public String process(String arg0) throws Exception {
-        throw new IllegalArgumentException("foo");
+        if(++count > 12){
+            throw new IllegalArgumentException("foo");
+        }
+        return arg0;
     }
 
 
