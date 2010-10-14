e6fc4cf7d7eb7f0de7f4f6a0189a524211c09e9a
Morten Andersen-Gott <morten.andersengott@gmail.com>
2010-10-14 22:09:55 +0200

    first draft for sql executing tasklet

--- src/main/java/no/magott/spring/batch/tasklet/SqlExecutingTasklet.java
+++ src/main/java/no/magott/spring/batch/tasklet/SqlExecutingTasklet.java
@@ -1,5 +1,7 @@
 package no.magott.spring.batch.tasklet;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.StepContribution;
@@ -10,35 +12,48 @@ import org.springframework.batch.item.ItemStream;
 import org.springframework.batch.item.ItemStreamException;
 import org.springframework.batch.item.util.ExecutionContextUserSupport;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SqlExecutingTasklet implements Tasklet, ItemStream {
 
-    private DataSource dataSource;
+    private final static String EXECUTION_COUNT = "sql.execution.count";
+	
+	private JdbcTemplate jdbcTemplate;
     private ExecutionContextUserSupport ecSupport;
+    private List<String> sqls;
+    private int count = 0;
+    
+    public SqlExecutingTasklet() {
+		ecSupport = new ExecutionContextUserSupport(SqlExecutingTasklet.class.getSimpleName());
+	}
     
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        return null;
-        
+        String sql = sqls.get(count);
+        int updateCount = jdbcTemplate.update(sql);
+        contribution.incrementWriteCount(updateCount);
+        return count++ > sqls.size() ? RepeatStatus.FINISHED : RepeatStatus.CONTINUABLE;
     }
 
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void close() throws ItemStreamException {
-        // TODO Auto-generated method stub
-        
+    	count = 0;
     }
 
     public void open(ExecutionContext executionContext) throws ItemStreamException {
-        // TODO Auto-generated method stub
-        
+    	if(executionContext.containsKey(ecSupport.getKey(EXECUTION_COUNT))){
+    		count = executionContext.getInt(ecSupport.getKey(EXECUTION_COUNT));
+    	}
     }
 
     public void update(ExecutionContext executionContext) throws ItemStreamException {
-        // TODO Auto-generated method stub
-        
+        executionContext.putInt(ecSupport.getKey(EXECUTION_COUNT), count);        
     }
 
+	public void setSqls(List<String> sqls) {
+		this.sqls = sqls;
+	}
 
 }
